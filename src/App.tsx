e66372cf4c230adfdba199e4959fe506ff117fcc
Sayuri <sayurikamble123@gmail.com>
2024-02-08 20:33:04 +0530

    Implemented `debounce` to trigger api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
+import { useRef } from "react";
 import "./App.css";
 
-function App() {
-  return <div></div>;
+function App(): JSX.Element {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const debounce = <T extends (...args: any[]) => any>(
+    fn: T,
+    delay: number = 1000
+  ) => {
+    let timeoutId: number;
+    console.log(delay, "ddee");
+    return (...args: any[]) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    };
+  };
+
+  const handleSearch = debounce(async (searchQuery: string) => {
+    fetch("http://localhost:5000/generate-image", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: searchQuery }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          onChange={() => {
+            handleSearch(inputRef.current?.value);
+          }}
+          ref={inputRef}
+        />
+        <button>Enter</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
