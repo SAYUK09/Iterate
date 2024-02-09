bd2c33db5ebde4d0bed9b5572f29d2197186c4b5
Sayuri <sayurikamble123@gmail.com>
2024-02-09 20:54:21 +0530

    feat: display relevant products

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
+
+interface Product {
+  id: number;
+  icon: string;
+  title: string;
+  description: string;
+  category: string[];
+}
 
 function App(): JSX.Element {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [imgURL, setImgURL] = useState<string | null>(null);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [message, setMessage] = useState<string>("");
 
   const debounce = <T extends (...args: any[]) => any>(
     fn: T,
@@ -29,23 +40,66 @@ function App(): JSX.Element {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        setImgURL(data.image_url);
       })
       .catch((error) => {
         console.error("Error:", error);
+      });
+
+    fetch("http://localhost:5000/relevant-products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product_idea: searchQuery }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the response data
+        console.log(data);
+        if (data.products.length) {
+          setProducts(data.products);
+        } else {
+          setMessage(data.message);
+        }
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("There was a problem with the fetch operation:", error);
       });
   });
 
   return (
     <div>
+      <div className="border border-sky-300 p-4">
+        <div>
+          <input
+            type="text"
+            onChange={() => {
+              handleSearch(inputRef.current?.value);
+            }}
+            ref={inputRef}
+          />
+        </div>
+
+        {imgURL && <img width={50} height={50} src={imgURL} />}
+      </div>
+
       <div>
-        <input
-          type="text"
-          onChange={() => {
-            handleSearch(inputRef.current?.value);
-          }}
-          ref={inputRef}
-        />
-        <button>Enter</button>
+        {products.length ? (
+          <div>
+            {products.map((product) => {
+              return <div>{product.title}</div>;
+            })}
+          </div>
+        ) : (
+          <div>{message}</div>
+        )}
       </div>
     </div>
   );
