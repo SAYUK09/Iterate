a799873b5ec44935c2ced87021df457a1f426b67
Sayuri <sayurikamble123@gmail.com>
2024-02-10 16:17:06 +0530

    enhancement: style & loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
-import "./App.css";
 import { fetchProducts, generateImage } from "./util";
+import bulb from "./assets/bulb.svg";
+import Loader from "./components/Loader";
 
 interface Product {
   id: number;
@@ -15,6 +16,7 @@ function App(): JSX.Element {
   const [imgURL, setImgURL] = useState<string | null>(null);
   const [products, setProducts] = useState<Product[]>([]);
   const [message, setMessage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const debounce = <T extends (...args: any[]) => any>(
     fn: T,
@@ -32,6 +34,7 @@ function App(): JSX.Element {
 
   const handleSearch = debounce(async (searchQuery: string) => {
     try {
+      setLoading(true);
       const imgURL = await generateImage(searchQuery);
       setImgURL(imgURL);
 
@@ -44,34 +47,72 @@ function App(): JSX.Element {
       }
     } catch (error) {
       console.error("There was a problem with the search operation:", error);
+    } finally {
+      setLoading(false);
     }
   });
 
   return (
-    <div>
-      <div className="border border-sky-300 p-4">
-        <div>
-          <input
-            type="text"
-            onChange={() => {
-              handleSearch(inputRef.current?.value);
-            }}
-            ref={inputRef}
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="p-2 flex rounded-md my-4 justify-center items-center relative">
+        <input
+          className="border border-sky-300 p-2 text-2xl rounded-md pr-12 mr-1 outline-none focus:border-blue-500"
+          type="text"
+          onChange={() => {
+            handleSearch(inputRef.current?.value);
+          }}
+          ref={inputRef}
+        />
+        {loading && (
+          <div className="absolute right-2 top-1/2 transform -translate-y-1/2 mr-4">
+            <Loader />
+          </div>
+        )}
+        {!loading && imgURL && (
+          <img
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 w-10 h-10 rounded-md mr-2"
+            src={imgURL}
+            alt="icon"
           />
-        </div>
-
-        {imgURL && <img width={50} height={50} src={imgURL} />}
+        )}
       </div>
 
+      {loading && <Loader />}
       <div>
-        {products.length ? (
-          <div>
-            {products.map((product) => {
-              return <div>{product.title}</div>;
-            })}
+        {!loading && products && (
+          <div className="grid grid-cols-3 gap-4">
+            {products.map((product, index) => (
+              <div
+                key={index}
+                className="border border-gray-300 rounded-lg p-4 my-4 w-96 "
+              >
+                <div className="flex items-center pb-4 border-b border-gray-300">
+                  <img
+                    className="w-12 h-12 rounded-full mr-4"
+                    src={product.icon}
+                    alt="product-icon"
+                  />
+                  <p className="text-lg font-semibold">{product.title}</p>
+                </div>
+                <div>
+                  <p className="pt-4">{product.description}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ) : (
-          <div>{message}</div>
+        )}
+
+        {!loading && message && (
+          <div className="bg-yellow-100 text-2xl border border-yellow-300 text-yellow-800 p-4 rounded-md shadow-md flex items-center">
+            <img
+              className="mx-2"
+              width={50}
+              height={50}
+              src={bulb}
+              alt="bulb-svg"
+            />
+            {message}
+          </div>
         )}
       </div>
     </div>
